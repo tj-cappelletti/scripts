eff0c2c504a8a577a0490a152dde48ec3db9a559
TJ Cappelletti <--local>
2022-05-06 10:25:59 -0400

    Created script to check if repo has LFS

---
+++ PowerShell/github/Get-GitHubReposWithLfsEnabled.ps1
@@ -0,0 +1,55 @@
+
+# TODO: Algin to best practices
+# https://github.com/PoshCode/PowerShellPracticeAndStyle
+[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Organization,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $PersonalAccessToken
+)
+
+begin {
+    # TODO: Change repos to org after testing
+    $orgReposApiEndpoint = "https://api.github.com/users/$Organization/repos"
+
+    $headers = @{
+        Accept = "application/vnd.github.v3+json"
+        Authorization = "Token $PersonalAccessToken" 
+    }
+}
+
+process {
+    $reposWithGitLfs = New-Object System.Collections.ArrayList
+    $repos = Invoke-RestMethod -Uri $orgReposApiEndpoint -Method Get -Headers $headers
+
+    foreach ($repo in $repos){
+        $repoContentsApiEndpoint = "https://api.github.com/repos/$Organization/$($repo.name)/contents/.gitattributes"
+        
+        try {
+            $fileContent = Invoke-RestMethod -Uri $repoContentsApiEndpoint -Method Get -Headers $headers
+            
+            $webResponse = Invoke-WebRequest -Uri $fileContent.download_url -Headers $headers
+
+            $gitAttributesFile = $webResponse.Content
+
+            if($gitAttributesFile -match "filter=lfs") {
+                $reposWithGitLfs.Add("$Organization/$($repo.name)") | Out-Null
+            }
+        } catch {
+            if($_.Exception.Response.StatusCode.value__ -ne 404) {
+                Write-Error "Unable to fetch contents for the repo $($repo.name)"
+            }
+        }
+    }
+
+    $reposWithGitLfs | Out-File -FilePath "git-lfs-repos.txt"
+}
+
+end {
+
+}
